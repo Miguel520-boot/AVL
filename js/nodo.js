8e25fd1459b06f68c1d5809ac9949971d855f829
miguelosorioc <contactosoymiler@gmail.com>
2026-05-27 11:18:00 -0500

    actualizacion del js

--- js/nodo.js
+++ js/nodo.js
@@ -1,5 +1,69 @@
-let num = 5;
-let num3 = 4;
-console.log(num + num3);
+class Nodo {
 
-alert("Hola Mundo");
+    constructor(valor) {
+
+        this.valor = valor;
+
+        this.Izquierdo = null;
+
+        this.Derecho = null;
+
+        this.Altura = 1;
+    }
+
+    getIzquierda() {
+        return this.Izquierdo;
+    }
+
+    getDerecha() {
+        return this.Derecho;
+    }
+
+    setIzquierda(nodo) {
+        this.Izquierdo = nodo;
+    }
+
+    setDerecha(nodo) {
+        this.Derecho = nodo;
+    }
+}
+
+
+class AVL {
+
+    constructor() {
+
+        this.raiz = null;
+    }
+
+    agregarNodo() {
+
+        let dato = parseInt(
+            document.getElementById("numero").value
+        );
+
+        let nuevoNodo = new Nodo(dato);
+
+     
+        if(this.raiz == null) {
+
+            this.raiz = nuevoNodo;
+        }
+        else {
+
+            if(dato > this.raiz.valor) {
+
+                this.raiz.setDerecha(nuevoNodo);
+            }
+            else {
+
+                this.raiz.setIzquierda(nuevoNodo);
+            }
+        }
+
+        console.log(this.raiz);
+    }
+}
+
+const arbol = new AVL();
+
